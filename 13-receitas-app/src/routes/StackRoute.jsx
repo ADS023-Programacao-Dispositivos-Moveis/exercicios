a9dbf0ee59eb98e0a361d5edd16526ce07d44fa1
Sérgio Silva <sergio.b.silva@iesb.edu.br>
2025-05-06 23:29:00 -0300

    Corrigindo nome da pasta Screen

--- 13-receitas-app/src/routes/StackRoute.jsx
+++ 13-receitas-app/src/routes/StackRoute.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import HomeScreen from '../screens/HomeScreen';
