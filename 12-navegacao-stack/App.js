da8f0e609aad2687f5e0057df1f41071b917fb4f
Sérgio Silva <sergio.b.silva@iesb.edu.br>
2025-05-06 22:01:22 -0300

    Exercicio de navigation para lista de receitas

--- 12-navegacao-stack/App.js
+++ 12-navegacao-stack/App.js
@@ -1,16 +1,20 @@
-import { PaperProvider } from 'react-native-paper'
-import { NavigationContainer } from '@react-navigation/native'
-
-import StackRoutes from './src/routes/StackRoutes';
-
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <PaperProvider>
-      <NavigationContainer>
-        <StackRoutes />
-      </NavigationContainer>
-    </PaperProvider>
-
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
